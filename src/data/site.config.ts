40ebeb786f1f6ac11b22ab4545d76a8edbbb1530
dinesh <talwadkerdinesh@gmail.com>
2024-02-20 13:59:33 +0530

    config

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
+	author: 'sanity esports', // Site author
 	title: 'Sanity Esports Blog', // Site title.
 	description: 'sanity esports blog', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	lang: 'en',
+	ogLocale: 'en',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 10 // Number of posts per page
 }
