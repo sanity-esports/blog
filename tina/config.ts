036cdda756eb9b2f257523ad247c4fe0d3d34dde
dinesh <talwadkerdinesh@gmail.com>
2024-02-20 21:53:45 +0530

    tina + seo optimised

--- tina/config.ts
+++ tina/config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'tinacms'
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
-	branch: "main",
+	branch,
 	clientId: process.env.clientId, // Get this from tina.io
 	token: process.env.token, 
 
